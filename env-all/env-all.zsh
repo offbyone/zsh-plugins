3ea22ac9bb8ed0f22503e993bc5d520715cdd746
Chris Rose <offby1@offby1.net>
2015-02-28 07:50:22 -0800

    include git-extensions in the path.

--- env-all/env-all.zsh
+++ env-all/env-all.zsh
@@ -2,3 +2,11 @@ append_path PATH $HOME/git-scripts
 append_path PATH $HOME/bin
 append_path PATH $HOME/local/bin
 append_path PATH $HOME/.local/bin
+
+# add path entries for git extensions
+for ext in $HOME/extensions/git*; do
+    if [ -d $ext/bin ]; then
+        ext=$ext/bin
+    fi
+    append_path PATH $ext
+done
