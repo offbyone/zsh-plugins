e161ef00bdcee8c64e77a9bc70bb1647276406bc
Chris Rose <offby1@offby1.net>
2015-03-07 09:15:20 -0800

    Make docker setup less invasive

--- docker/docker.plugin.zsh
+++ docker/docker.plugin.zsh
@@ -1,1 +1,6 @@
-[ -f /usr/local/bin/boot2docker ] && eval `/usr/local/bin/boot2docker shellinit`
+function docker() {
+    echo "First use setup of docker..."
+    [ -f /usr/local/bin/boot2docker ] && eval `/usr/local/bin/boot2docker shellinit`
+    unset -f docker
+    docker "$@"
+}
