498f09ddc1e8aa73a5a093a27aa5275ebfee33ee
Chris Rose <offby1@offby1.net>
2015-06-06 09:04:53 -0700

    Try lib when loading git extensions if they don't have bin

--- env-all/env-all.zsh
+++ env-all/env-all.zsh
@@ -7,6 +7,8 @@ append_path PATH $HOME/.local/bin
 for ext in $HOME/extensions/git*; do
     if [ -d $ext/bin ]; then
         ext=$ext/bin
+    elif [ -d $ext/lib ]; then # no bin, but lib? Sure, we'll try that...
+        ext=$ext/lib
     fi
     append_path PATH $ext
 done
