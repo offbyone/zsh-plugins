e66b3bd63e7d875a404dbf49ed610b08397c92e4
Chris Rose <offby1@offby1.net>
2015-08-13 07:04:28 -0700

    Convert to docker-toolbox VM

--- docker/docker.plugin.zsh
+++ docker/docker.plugin.zsh
@@ -1,20 +1,20 @@
 function docker() {
     # first try docker-machine
-    if hash boot2docker 2>/dev/null; then
-        cmd=$(which boot2docker)
-        echo "Using boot2docker from ${cmd}"
-        unset -f docker
-        eval `${cmd} shellinit`
-        docker "$@"
-    elif hash docker-machine 2>/dev/null; then
+    if hash docker-machine 2>/dev/null; then
         cmd=$(which docker-machine)
         echo "Using docker-machine from ${cmd}"
         unset -f docker
         if (($+DOCKER_MACHINE_NAME)); then
             eval `$cmd env $DOCKER_MACHINE_NAME`
         else
-            eval `$cmd env boot2docker`
+            eval `$cmd env dev`
         fi
+        docker "$@"
+    elif hash boot2docker 2>/dev/null; then
+        cmd=$(which boot2docker)
+        echo "Using boot2docker from ${cmd}"
+        unset -f docker
+        eval `${cmd} shellinit`
         docker "$@"
     elif hash docker 2>/dev/null; then
         echo "Using docker from $(which docker)"
