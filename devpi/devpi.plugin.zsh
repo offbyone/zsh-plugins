750a509890f28bc40d920bb46ef810f06580607f
Chris Rose <offby1@offby1.net>
2016-06-03 10:48:08 -0700

    Detect launchd devpi and use that

--- devpi/devpi.plugin.zsh
+++ devpi/devpi.plugin.zsh
@@ -2,16 +2,19 @@
 # the pip environments needed to use it preferentially
 
 __setup_devpi() {
-    local _docker_machine=${DOCKER_MACHINE_NAME:-dev}
-    local _devpi_container=devpi
-    local _running=$(docker inspect --format="{{ .State.Running }}" $_devpi_container 2> /dev/null)
-    if [[ "$_running" != "true" ]]; then
-        # the container isn't running
-        return 1
-    fi
+    if launchctl list net.devpi 2>/dev/null >/dev/null; then
+        export PIP_INDEX_URL=http://localhost:3141/root/pypi/+simple/
+    else
+        local _devpi_container=devpi
+        local _running=$(docker inspect --format="{{ .State.Running }}" $_devpi_container 2> /dev/null)
+        if [[ "$_running" != "true" ]]; then
+            # the container isn't running
+            return 1
+        fi
 
-    # okay, we have this now
-    export PIP_INDEX_URL=http://$(docker-machine ip $_docker_machine):3141/root/pypi/+simple/
+        # okay, we have this now
+        export PIP_INDEX_URL=http://localhost:3141/root/pypi/+simple/
+    fi
 }
 
 __setup_devpi
