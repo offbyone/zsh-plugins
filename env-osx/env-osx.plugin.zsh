ebe39c41110f610bcc2958da6ec36da0a1f24dc5
Chris Rose <offby1@offby1.net>
2016-06-03 10:47:53 -0700

    Alias on OSX to use coreutils

--- env-osx/env-osx.plugin.zsh
+++ env-osx/env-osx.plugin.zsh
@@ -1,1 +1,11 @@
-# Stub
+#!/bin/zsh
+function have() {
+    which $1 &>/dev/null
+}
+
+# GNU utils
+have gls && alias ls='gls --color=auto'
+have gsed && alias sed=gsed && alias esed='gsed -r'
+have grm && alias rm=grm
+have gxargs && alias xargs=gxargs
+have greadlink && alias readlink=greadlink
