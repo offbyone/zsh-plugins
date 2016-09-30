44f36b54cadf70ab2d88ee356f91a24d29613071
Chris Rose <chrisros@amazon.com>
2016-09-30 08:53:58 -0700

    Use cached pythonbrew installer

--- pythonbrew/pythonbrew.plugin.zsh
+++ pythonbrew/pythonbrew.plugin.zsh
@@ -1,4 +1,8 @@
+HERE=$(unset CDPATH; cd `dirname $0`; pwd)
+__python_brew_offer_file=$HERE/offer_install.zsh
+
 function ensure_pythonbrew() {
+
     if [[ -d $HOME/.pythonz ]]; then
         __python_brew_dir=$HOME/.pythonz
         [[ -x $HOME/.pythonz/bin/pythonz ]] && return 0
@@ -11,7 +15,7 @@ function ensure_pythonbrew() {
         return 1
     fi
 
-    /usr/bin/env zsh $ZSH/plugins/pythonbrew/offer_install.zsh
+    /usr/bin/env zsh $__python_brew_offer_file
     return $?
 }
 
@@ -20,3 +24,5 @@ if ensure_pythonbrew; then
     unset __python_brew_dir
     export PATH
 fi
+
+unset __python_brew_offer_file
